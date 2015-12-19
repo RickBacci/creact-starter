60b610168f25c31c521f9189d6490e0774b7c8ba
Rick Bacci <rickbacci@gmail.com>
2015-12-19 11:16:42 -0700

    add display skills using map

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -4,23 +4,26 @@ var AllSkills = React.createClass({
   },
 
   componentDidMount() {
-    // $.getJSON(
-    //   '/api/v1/skills.json',
-    //   (response) => { console.table(response) }
-    // );
-
     $.getJSON(
       '/api/v1/skills.json',
       (response) => { this.setState({ skills: response }) }
     );
-
   },
 
   render() {
-    console.log(this.state);
-    return (
+    var skills = this.state.skills.map((skill) => {
+      return (
+        <div key={skill.id}>
+          <h3>{skill.name}</h3>
+          <p><strong>Level:</strong> {skill.level}</p>
+          <p>{skill.details}</p>
+        </div>
+      )
+    });
+
+    return(
       <div>
-        <h3>Hello from AllSkills!</h3>
+        {skills}
       </div>
     )
   }

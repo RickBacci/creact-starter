8ec4b6e8eb74dc9befe1300d57d5f995176525c8
Rick Bacci <rickbacci@gmail.com>
2015-12-20 19:01:18 -0700

    add new skill
    - moved state to body
    - saves skill
    - immediately appends skill to DOM

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,10 +1,26 @@
 var Body = React.createClass({
+
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON(
+      '/api/v1/skills.json',
+      (response) => { this.setState({ skills: response }) }
+    );
+  },
+
+  handleSubmit(skill) {
+    let newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState });
+  },
+
   render() {
     return (
       <div>
-        <h1>Body</h1>
-        <NewSkill />
-        <AllSkills />
+        <NewSkill handleSubmit={ this.handleSubmit } />
+        <AllSkills skills={this.state.skills} />
       </div>
     )
   }
